a62a4f9ed79a0f22342e15112c41e7efa5411585
Alvin <anguyen8613@gmail.com>
2020-03-19 14:54:39 -0700

    Add firebase user authentication

--- src/components/auth/SignIn.js
+++ src/components/auth/SignIn.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
-
+import {connect} from 'react-redux';
+import {signIn} from '../../store/actions/authActions'
 class SignIn extends Component {
   state = {
     email: '',
@@ -12,9 +13,11 @@ class SignIn extends Component {
   }
   handleSubmit = (e) => {
     e.preventDefault();
-    console.log(this.state);
+    this.props.signIn(this.state);
+
   }
   render() {
+    const {authError} = this.props;
     return (
       <div className="container">
         <form className="white" onSubmit={this.handleSubmit}>
@@ -29,6 +32,9 @@ class SignIn extends Component {
           </div>
           <div className="input-field">
             <button className="btn pink lighten-1 z-depth-0">Login</button>
+            <div>
+              {authError? <p>{authError}</p>: <p>Login Sucess</p>}
+            </div>
           </div>
         </form>
       </div>
@@ -36,4 +42,15 @@ class SignIn extends Component {
   }
 }
 
-export default SignIn
+const mapStateToProps = (state) => {
+  return {
+    authError: state.auth.authError
+  }
+}
+const mapDispatchToProps = (dispatch) => {
+  return {
+    signIn: (creds) => dispatch(signIn(creds))
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(SignIn)
